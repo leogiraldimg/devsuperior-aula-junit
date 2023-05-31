56dea03ab3724de87c7918411f112ca22d71434f
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-05-31 00:02:14 -0300

    refactor: apply factory pattern

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
     @Test
     public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L, 0.0);
+        Account acc = AccountFactory.createEmptyAccount();
 
         acc.deposit(amount);
 
@@ -20,7 +21,7 @@ public class AccountTests {
     @Test
     public void depositShouldDoNothingWhenNegativeAmount() {
         double expectedValue = 100.0;
-        Account acc = new Account(1L, expectedValue);
+        Account acc = AccountFactory.createAccount(expectedValue);
         double amount = -200.0;
 
         acc.deposit(amount);
